f561c9ade88d40013cf49b0f99cbb95548a6ea4d
matt-hulme <mattwhulme@gmail.com>
2025-01-09 19:18:56 -0700

    Holy crap it worked

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-
-declare module "@remix-run/node" {
-  interface Future {
-    v3_singleFetch: true;
-  }
-}
+import { defineConfig } from 'vite'
+import { vitePlugin } from '@remix-run/dev'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [
-    remix({
+    vitePlugin({
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
@@ -21,4 +15,4 @@ export default defineConfig({
     }),
     tsconfigPaths(),
   ],
-});
+})
